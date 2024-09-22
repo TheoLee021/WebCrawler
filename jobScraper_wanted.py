118cb040f10e511cc78af6201d5a89f4bec122c6
Theo Lee <theolee021@gmail.com>
2024-09-21 23:22:41 -0700

    Add comments

--- jobScraper_wanted.py
+++ jobScraper_wanted.py
@@ -8,18 +8,20 @@ jobs_db = []
 
 def scrape_page(search_url):
     print(f"Scrapping {search_url}...")
+    # Access the website using playwright for Scroll
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto(search_url)
-
+    # Scroll down five times
     for i in range(5):
         time.sleep(1)
         page.keyboard.down("End")
 
     content = page.content()
     p.stop()
-
+    # End Playwright
+    # Job Scraping using BS4
     soup = BeautifulSoup(content, "html.parser")
     jobs = soup.find_all("div", class_="JobCard_container__REty8")
     count = 1
@@ -36,8 +38,10 @@ def scrape_page(search_url):
         jobs_db.append(job_data)
         count += 1
 
+# Execute job scraping by each keyword
 for keyword in keywords:
     url_keyword = f"{url}/search?query={keyword}&tab=position"
     scrape_page(url_keyword)
-
+    
+# Display results
 print(jobs_db)
