82d56e25174fd51c5f2af4e9fc7e9de791d85e20
Theo Lee <theolee021@gmail.com>
2024-10-03 13:42:37 -0700

    Update jobScraper_wanted.py
    Updated to OOP!

--- jobScraper_wanted.py
+++ jobScraper_wanted.py
@@ -3,51 +3,70 @@ from playwright.sync_api import sync_playwright
 import time
 import csv
 
+class JobScraper:
+    def __init__(self, url, keywords):
+        self.url = url
+        self.keywords = keywords
+        self.jobs_db = []
+
+    def show_jobs(self):
+        print(self.jobs_db)
+    
+class Wanted(JobScraper):
+    def __init__(self, url, keywords):
+        super().__init__(url, keywords)
+
+    # Scraping
+    def scrape_page(self, search_url):
+        print(f"Scrapping {search_url}...")
+        # Access the website using playwright for Scroll
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
+            page = browser.new_page()
+            page.goto(search_url)
+            # Scroll down five times
+            for i in range(5):
+                time.sleep(1)
+                page.keyboard.down("End")
+            content = page.content()
+        # End Playwright
+        # Job Scraping using BS4
+        soup = BeautifulSoup(content, "html.parser")
+        jobs = soup.find_all("div", class_="JobCard_container__REty8")
+        for job in jobs:
+            link = f"{self.url}{job.find('a')['href']}"
+            title = job.find("strong", class_="JobCard_title__HBpZf").text
+            company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
+            job_data = {
+                "title": title,
+                "company_name": company_name,
+                "link": link,
+            }
+            self.jobs_db.append(job_data)
+
+    # Execute job scraping using each keyword
+    def scrape_keyword(self):
+        if isinstance(self.keywords, str):
+            self.keywords = [self.keywords]
+
+        for keyword in self.keywords:
+            url_keyword = f"{self.url}/search?query={keyword}&tab=position"
+            self.scrape_page(url_keyword)
+        
+    # Exporting CSV
+    def export_csv(self):
+        with open("jobs.csv", "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Title", "Company", "Link"])
+
+            for job in self.jobs_db:
+                writer.writerow(job.values())
+
+# Creating an instance of jobScraper
 url = "https://www.wanted.co.kr"
 keywords = ["python"]
-jobs_db = []
 
-def scrape_page(search_url):
-    print(f"Scrapping {search_url}...")
-    # Access the website using playwright for Scroll
-    p = sync_playwright().start()
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto(search_url)
-    # Scroll down five times
-    for i in range(5):
-        time.sleep(1)
-        page.keyboard.down("End")
-
-    content = page.content()
-    p.stop()
-    # End Playwright
-    # Job Scraping using BS4
-    soup = BeautifulSoup(content, "html.parser")
-    jobs = soup.find_all("div", class_="JobCard_container__REty8")
-    for job in jobs:
-        link = f"{url}{job.find('a')['href']}"
-        title = job.find("strong", class_="JobCard_title__HBpZf").text
-        company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
-        job_data = {
-            "title": title,
-            "company_name": company_name,
-            "link": link,
-        }
-        jobs_db.append(job_data)
-
-# Execute job scraping by each keyword
-for keyword in keywords:
-    url_keyword = f"{url}/search?query={keyword}&tab=position"
-    scrape_page(url_keyword)
-    
-# Display results
-print(jobs_db)
-
-# Exporting CSV
-file = open("jobs.csv", "w")
-writer = csv.writer(file)
-writer.writerow(["Title", "Company", "Link"])
-
-for job in jobs_db:
-    writer.writerow(job.values())
+wanted_scraper = Wanted(url, keywords)
+wanted_scraper.scrape_keyword()
+wanted_scraper.export_csv()
+wanted_scraper.show_jobs()
