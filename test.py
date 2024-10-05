a9d88e4fa8e04c9f29de92a6c6a785dc245dfb07
Doolyvader <149716119+Doolyvader@users.noreply.github.com>
2024-10-05 08:43:27 -0700

    100424

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-import requests
-import bs4
 
+#https://weworkremotely.com/remote-full-time-jobs
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://weworkremotely.com/remote-full-time-jobs"
+
+response = requests.get(url)
+#print(response.content)
+
+soup = BeautifulSoup(
+    response.content, "html.parser"
+)
+
+jobs = soup.find("div", id = "job_list").find_all("li")[1:-1] # <-cut the list, top&bottom useless
+
+#print(jobs)
+
+for job in jobs:
+    title = job.find("span",class_="title").text
+    company, position, region = job.find_all("span", class_="company")
+    company = company.text
+    position = position.text
+    region = region.text
+    print(title, company, position, region)
