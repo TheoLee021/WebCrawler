861cc459d957aefd3e673927de17f40488b657d5
Theo Lee <theolee021@gmail.com>
2024-10-08 12:17:09 -0700

    Update Jobscraper_remoteOK.py
    OOP structure updated

--- Jobscraper_remoteOK.py
+++ Jobscraper_remoteOK.py
@@ -1,22 +1,57 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
-Job_List = []
+class JobScraper:
+    def __init__(self, url, keywords):
+        self.url = url
+        self.keywords = keywords
+        self.job_list = []
+    # Print Results
+    def show_jobs(self):
+        print(self.job_list)
 
-class Scraping:
-    def __init__(self, url):
-        self.response = requests.get(url, headers={"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"})
-        soup = BeautifulSoup(self.response.content, "html.parser")
+class RemoteOK(JobScraper):
+    def __init__(self, url, keywords):
+        super().__init__(url, keywords)
+
+    # Scraping
+    def scraping(self, url):
+        print(f"Scapping {url}")
+        response = requests.get(url, headers={"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"})
+        soup = BeautifulSoup(response.content, "html.parser")
         jobs = soup.find("table", id = "jobsboard").find_all("tr", class_ = "job")
         for x in jobs:
             title = x.find("h2", itemprop = "title").text
             location = x.find("div", class_ = "location").text
-            Job_List.append((title, location))
+            job_data = {
+                "title": title,
+                "location": location,
+            }
+            self.job_list.append(job_data)
+            
+    # Execute job scraping using each keyword
+    def scrape_keyword(self):
+        if isinstance(self.keywords, str):
+            self.keywords = [self.keywords]
 
-keyword = ["flutter", "python", "golang"]
+        for keyword in self.keywords:
+            url_keyword = f"{self.url}/remote-{keyword}-jobs"
+            self.scraping(url_keyword)
+        
+    # Exporting CSV
+    def export_csv(self):
+        with open("jobs.csv", "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Title", "Company", "Link"])
 
+            for job in self.job_list:
+                writer.writerow(job.values())
 
-for x in range(len(keyword)):
-    url = f"https://remoteok.com/remote-{keyword[x]}-jobs"
-    remote = Scraping(url)
-    print(Job_List)
+url = "https://remoteok.com"
+keywords = ["flutter", "python", "golang"]
+
+remoteok_scraper = RemoteOK(url, keywords)
+remoteok_scraper.scrape_keyword()
+remoteok_scraper.export_csv()
+remoteok_scraper.show_jobs()
